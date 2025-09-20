3bb6d41ce4a75a3cd694fb4ec0cfd05c1b88ed1f
Hxrxh <harshbhardwaj4556>
2025-09-21 02:29:27 +0530

    added sql with express

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
 const express = require("express");
 const app = express();
-const mySql = require("mysql2");
-
-const connection = mySql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "lenovog5012345",
-  database: "testdb",
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.log(err);
-
-    return;
-  }
-
-  console.log("connection  has beenn created.");
-});
-const connectionQuery = `create table Students(
-  id int AUTO_INCREMENT PRIMARY KEY,
-  name VARCHAR(20),
-  email VARCHAR(20))`;
-connection.execute(connectionQuery, (err) => {
-  if (err) {
-    console.log(err);
-    connection.end();
-    return;
-  }
-  console.log("table has been created.");
-});
-app.get("/", (req, res) => {
-  res.send("hello world");
-});
-
+const userRouter = require("./routes/userRouter");
+app.use(express.json());
+app.use("/users", userRouter);
 app.listen(3000, () => {
   console.log("Server running");
 });
